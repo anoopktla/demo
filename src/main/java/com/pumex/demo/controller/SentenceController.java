a58969d4e43f67a3bf3affbded9214bb6f4a5090
Anoop K <anoopktla@gmail.com>
2023-10-11 15:18:45 +0530

    refactored controller class

--- src/main/java/com/pumex/demo/controller/SentenceController.java
+++ src/main/java/com/pumex/demo/controller/SentenceController.java
@@ -2,29 +2,34 @@ package com.pumex.demo.controller;
 
 import com.pumex.demo.models.OpenAIResponse;
 import com.pumex.demo.service.OpenAIService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/demo")
 @RequiredArgsConstructor
 public class SentenceController {
 
-    private final OpenAIService openAIService;
+  private final OpenAIService openAIService;
 
-    @GetMapping("/text/correctedsentences")
-    public List<String> correctSentence(@RequestParam String input) {
-        OpenAIResponse response = openAIService.correctSentence(input);
-        return response.getChoicesList().stream().map(c -> c.getText().strip()).collect(Collectors.toList());
-    }
+  @GetMapping("/text/correctedsentences")
+  public List<String> correctSentence(@RequestParam String input) {
+    OpenAIResponse response = openAIService.correctSentence(input);
+    return response.getChoicesList().stream()
+        .map(choices -> choices.getText().strip())
+        .toList();
+  }
 
-    @GetMapping("/text/translatedsentences")
-    public List<String> translatedSentence(@RequestParam String input) {
-        OpenAIResponse response = openAIService.translatedSentence(input);
-        return response.getChoicesList().stream().map(c -> c.getText().strip()).collect(Collectors.toList());
-    }
-
+  @GetMapping("/text/translatedsentences")
+  public List<String> translatedSentence(@RequestParam String input) {
+    OpenAIResponse response = openAIService.translatedSentence(input);
+    return response.getChoicesList().stream()
+        .map(choices -> choices.getText().strip())
+        .toList();
+  }
 }
